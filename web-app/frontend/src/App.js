08ab78a6190b6a510e0263466e5b03fa103839c1
debajyotidasgupta <debajyotidasgupta6@gmail.com>
2021-03-19 22:30:24 +0530

    added landing route

--- web-app/frontend/src/App.js
+++ web-app/frontend/src/App.js
@@ -5,18 +5,18 @@ import './App.css';
 import Nav from './components/Nav'
 import Landing from './components/Landing';
 import Login from './components/Login'
-import {BrowserRouter as Router,Route} from 'react-router-dom'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Signup from './components/Signup'
 
 function App() {
   return (
-  	<Router>
-    <div className="App">
-      <Nav />
-      <Route exact path='/login' component={Login}></Route>
-      <Route exact path="/signup" component={Signup}></Route>
-      <Landing />
-    </div>
+    <Router>
+      <div className="App">
+        <Nav />
+        <Route exact path='/login' component={ Login }></Route>
+        <Route exact path="/signup" component={ Signup }></Route>
+        <Route exact path="/" component={ Landing }></Route>
+      </div>
     </Router>
   );
 }
