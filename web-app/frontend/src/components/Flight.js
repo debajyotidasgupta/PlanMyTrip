2282a29f10bbd47b8e51a64d19543418409acf25
somnathjena2011 <somnathjena.2011@gmail.com>
2021-03-21 16:34:01 +0530

    flight search

--- web-app/frontend/src/components/Flight.js
+++ web-app/frontend/src/components/Flight.js
@@ -3,6 +3,7 @@ import '../styles/Flight.scss'
 import {Button} from '@material-ui/core'
 import FlightTakeoffIcon from '@material-ui/icons/FlightTakeoff';
 import FlightLandIcon from '@material-ui/icons/FlightLand';
+import FlightIcon from '@material-ui/icons/Flight'
 
 export class Flight extends Component {
     render() {
@@ -11,13 +12,77 @@ export class Flight extends Component {
                 <div className="bg-flight"></div>
                 <div className="paper">
                     <form className="form-flight" noValidate>
-                        <div className="form-input">
-                            <datalist id="airports">
-                                <option>BBS</option>
-                                <option>CAL</option>
-                            </datalist>
-                            <input autoComplete="on" list="airports" placeholder="Depart From" style={{fontWeight:"bolder"}}/>
+                        <div className="row">
+                            <div className="column-left">
+                                <div className="form-input">
+                                    <datalist id="airports">
+                                        <option>BBS</option>
+                                        <option>CAL</option>
+                                    </datalist>
+                                    <input autoComplete="on" list="airports" placeholder="Departure" style={{fontWeight:"bolder"}}/>
+                                </div>
+                            </div>
+                            <div className="column-right">
+                                <span><FlightTakeoffIcon
+                                style={{fontSize:40,color:"#131a2f",
+                                }}/></span>
+                            </div>
                         </div>
+                        <div className="row">
+                            <div className="column-left">
+                                <div className="form-input">
+                                    <datalist id="airports">
+                                        <option>BBS</option>
+                                        <option>CAL</option>
+                                    </datalist>
+                                    <input autoComplete="on" list="airports" placeholder="Arrival" style={{fontWeight:"bolder"}}/>
+                                </div>
+                            </div>
+                            <div className="column-right">
+                                <span><FlightLandIcon
+                                style={{fontSize:40,color:"#131a2f",
+                                }}/></span>
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="column">
+                                <div class="form-input">
+                                    <input type="date" name="dob" required label="Date-of-birth"/>
+                                    <label style={{color:"#131a2f"}}>Date of Journey</label>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="column">
+                                <div class="form-input">
+                                    <select>
+                                        <option value="" disabled selected>Select Ticket Class</option>
+                                        <option value="premium-business">Premium Business</option>
+                                        <option value="business">Business</option>
+                                        <option value="first-class">First Class</option>
+                                        <option value="economy">Economy</option>
+                                    </select>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="column">
+                                <div class="form-input">
+                                    <input type="number" min="0" name="no-of-passengers" required/>
+                                    <label>No of passengers</label>
+                                </div>
+                            </div>
+                        </div>
+                        <Button
+                        type="submit"
+                        fullWidth
+                        variant="contained"
+                        color="primary"
+                        className="submit"
+                        endIcon={<FlightIcon/>}
+                        >
+                            Search Flights
+                        </Button>
                     </form>
                 </div>
             </div>
