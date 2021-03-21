fd986f0aeec00c7c2a2e762e01fb15fbfc4cf2b7
somnathjena2011 <somnathjena.2011@gmail.com>
2021-03-21 15:15:38 +0530

    departfrom visible

--- web-app/frontend/src/components/Flight.js
+++ web-app/frontend/src/components/Flight.js
@@ -1,48 +1,26 @@
 import React, { Component } from 'react'
 import '../styles/Flight.scss'
-import {Button,TextField} from '@material-ui/core'
+import {Button} from '@material-ui/core'
 import FlightTakeoffIcon from '@material-ui/icons/FlightTakeoff';
 import FlightLandIcon from '@material-ui/icons/FlightLand';
-import Autocomplete from '@material-ui/lab/Autocomplete'
-
-const airports = [
-    {id: 'BBS', city: 'Bhubaneswar'},
-    {id: 'CAL', city: 'Kolkata'},
-];
 
 export class Flight extends Component {
-    constructor(){
-        super();
-        this.state = {
-            id: 'BBS',
-            city: 'Bhubaneswar',
-        }
-    }
-
     render() {
-    return (<Autocomplete
-                        options={airports}
-                        getOptionLabel={option=>option.city}
-                        id="airports"
-                        value={this.state.city}
-                        onChange={(event,newAirport)=>{
-                            if(newAirport){
-                                return this.setState(
-                                    {
-                                        id: newAirport.id,
-                                        city: newAirport.city, 
-                                    }
-                                );
-                            }
-                        }}
-                        renderInput={params=>{
-                            <TextField {...params} label="From" margin="normal"/>
-                        }}
-                        style={{height: 300, 
-                            opacity:1.0,
-                            visibility: 1,
-                        }}
-                        />
+        return(
+            <div className="main">
+                <div className="bg-flight"></div>
+                <div className="paper">
+                    <form className="form-flight" noValidate>
+                        <div className="form-input">
+                            <datalist id="airports">
+                                <option>BBS</option>
+                                <option>CAL</option>
+                            </datalist>
+                            <input autoComplete="on" list="airports" placeholder="Depart From" style={{fontWeight:"bolder"}}/>
+                        </div>
+                    </form>
+                </div>
+            </div>
         );
     }
 }
