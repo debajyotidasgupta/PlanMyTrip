725ad2656ae47b8c823a446f545ed9b21c03912d
somnathjena2011 <somnathjena.2011@gmail.com>
2021-03-23 16:26:32 +0530

    tried

--- web-app/frontend/src/components/Login.js
+++ web-app/frontend/src/components/Login.js
@@ -1,21 +1,55 @@
 import React, { Component } from 'react'
 import '../styles/Login.scss'
 import {Button} from '@material-ui/core'
+import axios from 'axios'
+import { Redirect } from 'react-router-dom'
 
 export class Login extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            email: '',
+            password: '',
+        }
+    }
+    handleEmailChange= e=>{
+        this.setState({
+            email: e.target.value,
+        });
+    }
+    handlePasswordChange= e=>{
+        this.setState({
+            password: e.target.value,
+        });
+    }
+    handleSubmit = e=>{
+        e.preventDefault();
+        const user = {
+            email: this.state.email,
+            password: this.state.password,
+        }
+        
+        axios.post('http://127.0.0.1:5000/auth/login',{user},{withCredentials: true}).then(res=>{
+            console.log("Pakistan")
+            console.log(res.body);
+            return(
+                <Redirect to={"/light/results"}/>
+            );
+        })
+    }
     render(){
     return (
         <div class="main">
             <div className="bg">
             </div>
             <div className="paper">
-                <form className="form-login" noValidate>
+                <form className="form-login" onSubmit={this.handleSubmit} noValidate>
                     <div class="form-input">
-                        <input type="email" name="email" required/>
+                        <input type="email" name="email" onChange={this.handleEmailChange} required/>
                         <label>Email address</label>
                     </div>
                     <div class="form-input">
-                        <input type="password" name="password" required/>
+                        <input type="password" name="password" onChange={this.handlePasswordChange} required/>
                         <label>Password</label>
                     </div>
                     <Button
