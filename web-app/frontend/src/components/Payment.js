ff4a523b1bca4479396f7d4b957e7bcf50024ea3
debajyotidasgupta <debajyotidasgupta6@gmail.com>
2021-03-30 12:37:02 +0530

    payment

--- web-app/frontend/src/components/Payment.js
+++ web-app/frontend/src/components/Payment.js
@@ -1,13 +1,80 @@
-import React, { Component } from 'react'
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Radio from '@material-ui/core/Radio';
+import RadioGroup from '@material-ui/core/RadioGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormControl from '@material-ui/core/FormControl';
+import FormHelperText from '@material-ui/core/FormHelperText';
+import FormLabel from '@material-ui/core/FormLabel';
+import Button from '@material-ui/core/Button';
 
-export class Payment extends Component {
-    render() {
-        return (
-            <div>
-                
-            </div>
-        )
-    }
+const useStyles = makeStyles((theme) => ({
+    body: {
+        display:'flex',
+        width: '100%',
+        height: '100vh',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    heading:{
+        fontSize: '3em',
+        fontWeight: '700',
+        color: 'black'
+    },
+    formControl: {
+        height: '80%',
+        width: '40%',
+        margin: 'auto',
+        marginTop: '110px',
+        boxShadow: '5px 10px 18px #fe435b',
+        boxSizing: 'border-box',
+        padding: '50px'
+    },
+    button: {
+        margin: theme.spacing(1, 1, 0, 0),
+    },
+}));
+
+export default function Payment() {
+    const classes = useStyles();
+    const [value, setValue] = React.useState('');
+    const [error, setError] = React.useState(false);
+    const [helperText, setHelperText] = React.useState('Choose wisely');
+
+    const handleRadioChange = (event) => {
+        setValue(event.target.value);
+        setHelperText(' ');
+        setError(false);
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        if (value === 'best') {
+            setHelperText('You got it!');
+            setError(false);
+        } else if (value === 'worst') {
+            setHelperText('Sorry, wrong answer!');
+            setError(true);
+        } else {
+            setHelperText('Please select an option.');
+            setError(true);
+        }
+    };
+
+    return (
+        <form onSubmit={ handleSubmit } className={ classes.body }>
+            <FormControl component="fieldset" error={ error } className={ classes.formControl }>
+                <FormLabel component="legend" className={ classes.heading }>PAYMENT POTRAL</FormLabel>
+                <RadioGroup aria-label="quiz" name="quiz" value={ value } onChange={ handleRadioChange }>
+                    <FormControlLabel value="nbank" control={ <Radio /> } label="Net Banking" />
+                    <FormControlLabel value="card" control={ <Radio /> } label="Credit/Debit Card" />
+                </RadioGroup>
+                <FormHelperText>{ helperText }</FormHelperText>
+                <Button type="submit" variant="outlined" color="primary" className={ classes.button }>
+                    Make Payment
+        </Button>
+            </FormControl>
+        </form>
+    );
 }
-
-export default Payment
